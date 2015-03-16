f60ebf830f151ccdb45ca876de0f8cd0ca137c91
Joel Pagliuca <JoelPagliuca@hotmail.com>
2015-03-16 11:38:32 +1100

    cleaned EntityManager

--- TramSim/Control/EntityManager.py
+++ TramSim/Control/EntityManager.py
@@ -7,11 +7,16 @@ Created on 16/03/2015
 class EntityManager:
     '''
     Will be controlling most of the Entities
+    
+    PROPERTIES
+        _interface: Interfaces.UserInterface - will be rendering information
+        _map: Locations.Map - the map for the game
     '''
 
 
-    def __init__(self, interface):
+    def __init__(self, interface, map_):
         '''
         Constructor
         '''
-        self.interface = interface
+        self._interface = interface
+        self._map = map_
